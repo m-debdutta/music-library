ef9b3b9d3b8162db65c8db4fd41fd81b6b071f94
Debdutta <m-debdutta@users.noreply.github.com>
2023-08-27 18:12:01 +0530

    Fix bug: application won't run if file is not created.

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const { createApp } = require('./src/app');
 const Playlists = require('./src/models/playlists');
 const { Storage } = require('./src/storage');
 
-const setUpStorageDir = () => {
+const setUpStorageDir = (playlistDataFilePath) => {
   if (!fs.existsSync('./data')) fs.mkdirSync('./data');
+  if (!fs.existsSync(playlistDataFilePath)) fs.writeFileSync(playlistDataFilePath, '[]');
 };
 
 const main = () => {
@@ -13,9 +14,10 @@ const main = () => {
   const playlistDataFilePath = './data/playlist-data.json';
   const playlists = new Playlists();
   const playlistStorage = new Storage(fs, playlistDataFilePath);
-  const app = createApp(playlists, playlistStorage);
 
-  setUpStorageDir();
+  setUpStorageDir(playlistDataFilePath);
+
+  const app = createApp(playlists, playlistStorage);
   app.listen(port, () => {
     console.log('Listening on port: ', port);
     console.log(new Date().toGMTString());
